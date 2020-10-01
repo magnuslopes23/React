a0c91e2d25dc598a598a01bd557b68dea085b905
magnuslopes23 <lopesmagnus2324@gmail.com>
2020-10-02 00:47:48 +0530

    single page application

--- src/components/HomeComponent.js
+++ src/components/HomeComponent.js
@@ -1,9 +1,32 @@
 import React from 'react';
+import {Card,CardBody,CardImg,CardText,CardTitle,CardSubtitle} from 'reactstrap';
 
-const Home = () =>{
+const RenderCard = ({item})=>{
     return(
-        <div>
-            <h4>Home</h4>
+        <Card>
+            <CardImg src={item.image} alt={item.name} />
+            <CardBody>
+            <CardTitle>{item.name}</CardTitle>
+            {item.designation ? <CardSubtitle>{item.designation}</CardSubtitle> : null }
+            <CardText>{item.description}</CardText>
+            </CardBody>
+        </Card>
+    );
+}
+const Home = (props) =>{
+    return(
+        <div className='conatiner'>
+            <div className='row align-items-start'>
+                <div className='col-12 col-md m-1'>
+                    <RenderCard item={props.dish}/>
+                </div>
+                <div className='col-12 col-md m-1'>
+                    <RenderCard item={props.promotion}/>
+                </div>
+                <div className='col-12 col-md m-1'>
+                    <RenderCard item={props.leader}/>
+                </div>
+            </div>
         </div>
     )
 }
