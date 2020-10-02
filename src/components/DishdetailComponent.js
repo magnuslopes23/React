0a9f71f2803382e3a36721b3c07c5c8c228fa6b8
magnuslopes23 <lopesmagnus2324@gmail.com>
2020-10-02 15:40:11 +0530

    assignment-2

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -28,19 +28,19 @@ function RenderComments({comments}) {
         return(
             <div className="col-12 col-md-5 m-1">
                 <h4>Comments</h4>
-                <ul className="list-unstyled">
-                    {comments.map((comment)=>{
-                        return(
-                            <li key={comment.id} >
-                                <div>
-                                    <p>{comment.comment}</p>
-                                    <p>--{comment.author},
-                                    {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comment.date)))}</p>
-                                </div>
-                            </li>
-                        )
-                    })}
-                </ul>
+                        <ul className="list-unstyled">
+                            {comments.map((comment)=>{
+                                return(
+                                    <li key={comment.id} >
+                                        <div>
+                                            <p>{comment.comment}</p>
+                                            <p>--{comment.author},
+                                            {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comment.date)))}</p>
+                                        </div>
+                                    </li>
+                                )
+                            })}
+                        </ul>
             </div>
         )
     }
